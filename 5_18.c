fe6d88cf00818f95db21e3c6561f662b58730466
ZXL-13 <2985656280@qq.com>
2025-05-17 18:57:33 +0800

    Add files via upload

---
+++ 5_18.c
@@ -0,0 +1,10 @@
+#include<math.h>
+int main()
+{int result_1=2+10/8*3;
+double result_2=2+10/8*3;
+double result_3;
+result_3=sqrt(4)*2+pow(3,2);
+printf("result_1=%d\n",result_1);
+printf("result_2=%d\n",result_2);
+printf("%.9f\n",result_3);
+}
