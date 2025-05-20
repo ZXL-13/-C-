5d2bfd7bad798b31aed550a869a229763951f588
ZXL-13 <2985656280@qq.com>
2025-05-20 20:05:05 +0800

    Add files via upload

---
+++ 5_20_if.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a=2;
+	int b=8;
+	int result;
+	if(a>b)
+	{
+		result=a-b;
+		printf("a与b的差值为%d",result);
+	}
+else
+	{
+	result=b-a;
+	printf("a与b的差值为%d",result);
+}
+}
